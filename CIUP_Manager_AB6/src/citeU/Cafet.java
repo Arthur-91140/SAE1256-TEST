a016d5ec3cf6ab4bb0d159f41acf846fed22493a
Arthur Pruvost Rivière <96424611+Arthur-91140@users.noreply.github.com>
2025-05-30 16:01:41 +0200

    Refactor Cafet class to implement Serializable interface and add serialVersionUID

--- CIUP_Manager_AB6/src/citeU/Cafet.java
+++ CIUP_Manager_AB6/src/citeU/Cafet.java
@@ -1,12 +1,12 @@
 package citeU;
 
+import java.io.Serializable;
 
-public class Cafet extends Restauration {
+public class Cafet extends Restauration implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
 
-	public Cafet(String nom, int capacite) {
-
-		super(nom, capacite);
-
-	}
-
+    public Cafet(String nom, int capacite) {
+        super(nom, capacite);
+    }
 }
